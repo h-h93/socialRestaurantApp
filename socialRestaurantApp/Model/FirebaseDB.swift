4132a0f573933bcc42ad8da7404d97567aec5d86
h-93 <hanif234@hotmail.com>
2023-11-08 21:42:29 +0000

    adding firebase backend connectivity along with futrther UI design

---
+++ socialRestaurantApp/Model/FirebaseDB.swift
@@ -0,0 +1,8 @@
+//
+//  UserDB.swift
+//  socialRestaurantApp
+//
+//  Created by hanif hussain on 08/11/2023.
+//
+
+import Foundation
---
+++ socialRestaurantApp/ViewModel/CustomFriendTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomFriendTableViewCell.swift
+//  socialRestaurantApp
+//
+//  Created by hanif hussain on 08/11/2023.
+//
+
+import UIKit
+
+class CustomFriendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
